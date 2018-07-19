80164cb2287a0fcf8bde6f19c252b263b8379636
spayne <sean.d.payne@gmail.com>
2018-07-19 14:52:31 -0700

    use xcopy instead of copy in the install batch files in case the user doesn't have those directories already (thanks seiferth)

--- install_softknuckles_release.bat
+++ install_softknuckles_release.bat
@@ -1,2 +1,2 @@
 "D:\Program Files (x86)\Steam\steamapps\common\SteamVR\bin\win64\vrpathreg.exe" adddriver c:\projects\soft_knuckles\soft_knuckles
-copy x64\Debug\soft_knuckles.dll soft_knuckles\bin\win64\driver_soft_knuckles.dll
+echo f | xcopy x64\Debug\soft_knuckles.dll soft_knuckles\bin\win64\driver_soft_knuckles.dll
